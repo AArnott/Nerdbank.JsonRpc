a72edb4d521f38bb6d3f832c9350bc4bad8ef512
Andrew Arnott <andrewarnott@live.com>
2025-11-25 13:48:02 -0700

    Add Benchmarks project

---
+++ test/Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run(
+	typeof(Program).Assembly,
+	DefaultConfig.Instance/*.WithOptions(ConfigOptions.DisableOptimizationsValidator)*/);
